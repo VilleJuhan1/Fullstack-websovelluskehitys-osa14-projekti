87bcf5a5025dc7e8714f6db6c0457c9e28a92def
Ville-Juhani Nivasalo <ville.nivasalo@gmail.com>
2026-06-15 14:18:36 +0300

    DB model and migration for Dota Heroes

---
+++ App/backend/src/db/models/DotaHero.ts
@@ -0,0 +1,44 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../sequelize';
+
+export class DotaHero extends Model {
+  declare id: number;
+  declare name: string;
+  declare translations: Record<string, string> | null;
+  declare categories: string[];
+  declare imageUrl: string;
+}
+
+DotaHero.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    translations: {
+      type: DataTypes.JSONB,
+      allowNull: true,
+    },
+    categories: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+      defaultValue: [],
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: false,
+    timestamps: true,
+    modelName: 'dotaHero',
+    tableName: 'dotaHero', // ensure pluralization doesn't mess it up
+  }
+);
