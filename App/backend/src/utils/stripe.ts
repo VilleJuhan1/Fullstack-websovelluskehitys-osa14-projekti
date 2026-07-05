0c9e4d33a5d0407a0a035ffc36a106618be2e0a4
Ville-Juhani Nivasalo <ville.nivasalo@gmail.com>
2026-07-05 08:41:41 +0300

    newer versions

--- App/backend/src/utils/stripe.ts
+++ App/backend/src/utils/stripe.ts
@@ -7,18 +7,18 @@ const stripeSecretKey = process.env.STRIPE_SECRET_KEY || 'sk_test_12345';
  * Configured with the API version and optionally customized host, protocol, and port from environment variables for testing.
  */
 export const stripe = new Stripe(stripeSecretKey, {
-  apiVersion: '2026-05-27.dahlia', // Use latest required by SDK typings
+  apiVersion: '2026-06-24.dahlia', // Use latest required by SDK typings
   host: process.env.STRIPE_API_BASE
     ? process.env.STRIPE_API_BASE.replace(/^https?:\/\//, '').split(':')[0]
     : 'api.stripe.com',
   protocol:
     process.env.STRIPE_API_BASE &&
-    process.env.STRIPE_API_BASE.startsWith('http://')
+      process.env.STRIPE_API_BASE.startsWith('http://')
       ? 'http'
       : 'https',
   port:
     process.env.STRIPE_API_BASE &&
-    process.env.STRIPE_API_BASE.split(':').length === 3
+      process.env.STRIPE_API_BASE.split(':').length === 3
       ? parseInt(process.env.STRIPE_API_BASE.split(':')[2])
       : undefined,
 });
