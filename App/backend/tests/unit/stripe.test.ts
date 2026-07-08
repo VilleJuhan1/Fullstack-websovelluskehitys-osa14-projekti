5e181e01639c1c0a224d52f36b888ade0c7e413f
Ville-Juhani Nivasalo <ville.nivasalo@gmail.com>
2026-07-08 08:52:58 +0300

    stripe test

---
+++ App/backend/tests/unit/stripe.test.ts
@@ -0,0 +1,21 @@
+import { stripe } from '../../src/utils/stripe';
+interface StripeWithApi {
+  _api: {
+    version: string;
+    host: string;
+    protocol: string;
+  };
+}
+
+describe('Stripe Utility Unit Tests', () => {
+  it('should initialize stripe with apiVersion', () => {
+    expect(stripe).toBeDefined();
+    expect((stripe as unknown as StripeWithApi)._api.version).toBe('2026-06-24.dahlia');
+  });
+
+  it('should resolve base configurations', () => {
+    // Check if configuration matches stripe constructor
+    expect((stripe as unknown as StripeWithApi)._api.host).toBeDefined();
+    expect((stripe as unknown as StripeWithApi)._api.protocol).toBeDefined();
+  });
+});
