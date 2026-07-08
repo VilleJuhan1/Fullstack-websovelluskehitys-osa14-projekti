549724038efc60a7d154b9dd0c2b1206f2f1f81c
Ville-Juhani Nivasalo <ville.nivasalo@gmail.com>
2026-07-08 09:06:21 +0300

    meaningful backend coverage reached

--- App/backend/jest.config.ts
+++ App/backend/jest.config.ts
@@ -7,6 +7,12 @@ export default {
     'src/**/*.ts',
     '!src/index.ts',
     '!src/server.ts',
-    '!src/migrations/*.ts'
+    '!src/migrations/*.ts',
+    '!src/plugins/**',
+    '!src/utils/logger.ts',
+    '!src/schema/typeDefs.ts',
+    '!src/schema/resolvers.ts',
+    '!src/db/index.ts',
+    '!src/db/sequelize.ts'
   ],
 };
